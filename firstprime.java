1b5e1c7496cb30f7b58f2ec29daec8d1e9e4e48f
kk-linden <77739454+kk-linden@users.noreply.github.com>
2022-05-27 20:34:18 -0400

    Add files via upload

---
+++ firstprime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+class firstprime {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        int i = 0;
+        int num = 0;
+        String primeNumbers = "";
+        for (i = 1; i <= 100000; i += 2) {
+            int counter = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+                primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        System.out.println("Prime numbers from 1 to 100000 are:");
+        System.out.println(2 + " " + primeNumbers);
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
